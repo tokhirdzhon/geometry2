e13fd0754d762e660aa71fd8032a01eb21bf2be0
tokhirdzhon <thefrankbest@gmail.com>
2020-02-29 11:05:38 +0700

    Input and output circle

--- main.c
+++ main.c
@@ -1,13 +1,82 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+
+void delete (char str[128], int n)
+{
+    for (int i = n; i < strlen(str); i++) {
+        str[i] = str[i + 1];
+    }
+}
+
 int main()
 {
-    int x, y, r;
-    float perimetr;
+    char figure[64] = "circle";
+    char input[128], name[64];
+    fgets(input, 128, stdin);
+    input[strlen(input) - 1] = '\0';
 
-    printf("\nInput center(x, y) and radius separated by space: ");
-    scanf("%i%i%i", &x, &y, &r);
-    printf("\ncircle(%i %i, %i)", x, y, r);
+    int test = 1;
+    // Удаление лишних пробелов
+    for (int j = 0, i = 0; i < strlen(input); i++) {
+        if (input[i] == ' ') {
+            if (!(((input[i - 1] == ',') && isdigit(input[i + 1])) ||
+                  (isdigit(input[i - 1]) && isdigit(input[i + 1])))) {
+                delete (input, i--);
+            }
+        }
+        else if (isalpha(input[i]) && test) {
+            name[j++] = input[i];
+            name[j] = '\0';
+        }
+        else {
+            test = 0;
+        }
+    }
 
-    perimetr = 2 * 3.14 * r;
-    printf("\n  perimetr = %.2f\n\n", perimetr);
+    // Проверка названия фигуры
+    if (strcmp(name, figure)) {
+        printf("Error: такой фигуры не существует");
+    }
+    else {
+        if ((input[strlen(name)] != '(') || (input[strlen(input) - 1] != ')')) {
+            printf("Error: ошибка в синтаксисе");
+            return 0;
+        }
+
+        int comma = 0, space = 0;
+        for (int i = 0; i < strlen(input); i++) {
+            if ((input[i] == '.') && (!isdigit(input[i - 1]) || !isdigit(input[i + 1]))) {
+                printf("Error: ошибка в синтаксисе");
+                return 0;
+            }
+            if (input[i] == ',') {
+                comma++;
+            }
+            if (input[i] == ' ') {
+                space++;
+            }
+        }
+        if ((comma != 1) || (space != 2)) {
+            printf("Error: invalid circle");
+            return 0;
+        }
+
+        int arg = 0;
+        // Проверка на правильность аргументов
+        for (int i = strlen(name) + 1; i < strlen(input) - 1; i++) {
+            if (isdigit(input[i])) {
+                arg++;
+                for (int j = i; isdigit(input[j]); j++, i++)
+                    ;
+                if ((arg == 1) && (input[i] != ' ')) {
+                    printf("Error: invalid circle");
+                    return 0;
+                }
+            }
+        }
+        puts(input);
+    }
+
+    return 0;
 }
